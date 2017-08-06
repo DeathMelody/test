c412a241ed1d0565e72990b61b94a8f3c76df5b6
DeathMelody <515102624@qq.com>
2017-08-06 22:01:22 +0800

    math方法

--- T1.js
+++ T1.js
@@ -30,9 +30,7 @@
 // var s = [one,two,three] = foo;
 // console.log(s)
 //进制？
-var c = function (s){
-    console.log(s)
-}
+var c = function (x){console.log(x)}
 // var sixteen = 0xABCDDDDD;//十六进制以0x开头，不论大小写
 // var ten = 0900101010;//十进制0后面跟>=8的数字
 // var eight = 076666666;//八进制0后面跟随0到7（包含7）的数字
@@ -62,15 +60,39 @@ var c = function (s){
 // var dou = Number.parseFloat(str)
 // c(dou)
 // Number.parseFloat
-var arr = [1,2,3,4,5];
-var f =
+// var arr = [1,2,3,4,5];
+// var f =
     // var arr1 = [];
     // for(var i = 0; i<x.length;i++){
     //     arr1.push(x[i]+0.5);
     // }
     // arr1.pop();
     // return arr1;
-    arr.map(function(item) {
-       return item+0.5;
-    }).pop();
-c(f)//[1.5,2.5,3.5,4.5]
+//     arr.map(function(item) {
+//        return item+0.5;
+//     }).pop();
+// c(f)//[1.5,2.5,3.5,4.5]
+
+// const zipWith = (f,[x, ...xs],[y, ...ys])=>
+// x == void 0 || y == void 0
+// ?[]
+// :[f(x,y), ...zipWith(f,xs,ys)]
+
+var i = 0b0101010001010110;
+
+//number和math方法
+
+// c(Number.isFinite(i));//是否为有限数字
+// c(Number.isInteger(i));//是否为整数
+// c(Number.isSafeInteger(i))//值是否为安全整数
+// c(Math.PI);//π
+// c(Math.sin(1.56));//三角函数？？？
+// c(Math.floor(123));
+// c(Math.min(1,2));//返回逗号间隔的较小的值，max()返回较大的值
+c(parseInt(100*Math.random()));
+//四舍五入                   数字的最接近的单精度浮点数表示       截断小数点后面的数
+c("round:"+Math.round(4.5)+",fround:"+Math.fround(4.4)+",trunc:"+Math.trunc(4.5));
+c("sqrt:"+Math.sqrt(9)+",cbrt:"+Math.cbrt(9)+",hypot:"+Math.hypot(9));//平方根，立方根，第三没看懂
+    
+
+
